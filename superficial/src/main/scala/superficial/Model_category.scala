e21384957f9f2ff1b4bf8b5c1bd7d8720cd7e0bf
arkaghosh <arkasparko@gmail.com>
2021-04-04 20:39:47 +0200

    Added axioms CM2 and CM3 from Goerss and Jardin

--- superficial/src/main/scala/superficial/Model_category.scala
+++ superficial/src/main/scala/superficial/Model_category.scala
@@ -10,14 +10,105 @@ trait Model extends Category { model =>
   var weak_eqv  : Set[Morphism]
 
   /**
-   *    f.domain
-   *       |
-   *     i |
-   *       |
-   *       \/
-   *       c --------> f.range
-   *             p
+   * Axiom CM2 from Goerss and Jardin
    */
+  def axiom_2(f : Morphism, g : Morphism, h : Morphism) : Unit = {  
+
+    require(model.morphisms.contains(f), s"$f is not a morphism in the category $model")
+    require(model.morphisms.contains(g), s"$g is not a morphism in the category $model")
+    require(model.morphisms.contains(h), s"$h is not a morphism in the category $model")
+    
+    require(model.are_equal(f.+(g), h) , s"$f composed with $g is not equal to $h")
+    
+    require(model.weak_eqv.intersect(Set(f, g, h)).size >= 2,
+      s"No two of $f, $g and $h are weak equivalences")
+
+    model.weak_eqv += f
+    model.weak_eqv += g
+    model.weak_eqv += h   
+  }
+
+  /**
+   *  Axiom CM3 from Goerss and Jardin
+   *
+   *          i           r
+   *      X -------> X' -------> X
+   *      |          |           |
+   *    f |        g |         f |
+   *      |          |           |
+   *      \/         \/          \/
+   *      Y -------> Y' -------> Y
+   *           i           r
+   *      
+   *      r . i = id
+   */
+  def axiom_3_weak_eqv(f : Morphism, g : Morphism, r : Morphism, i : Morphism) : Unit = {
+    require(model.morphisms.contains(f), s"$f is not a morphism in the category $model")
+    require(model.morphisms.contains(g), s"$g is not a morphism in the category $model")
+    require(model.morphisms.contains(i), s"$i is not a morphism in the category $model")
+    require(model.morphisms.contains(r), s"$r is not a morphism in the category $model")
+
+    require(model.are_equal(r.+(i), id_morph(f.domain)), s"$r composed with $i is not identity")
+    require(model.are_equal(i.+(f), g.+(i)), s"$g composed with $i is not same as $i composed with $f")
+    require(model.are_equal(r.+(g), f.+(r)), s"$r composed with $g is not same as $f composed with $r")
+
+    require(model.weak_eqv.contains(g), s"$g is not a weak equivalence in the category $model")
+
+    model.weak_eqv += f
+  }
+
+  def axiom_3_fib(f : Morphism, g : Morphism, r : Morphism, i : Morphism) : Unit = {
+    require(model.morphisms.contains(f), s"$f is not a morphism in the category $model")
+    require(model.morphisms.contains(g), s"$g is not a morphism in the category $model")
+    require(model.morphisms.contains(i), s"$i is not a morphism in the category $model")
+    require(model.morphisms.contains(r), s"$r is not a morphism in the category $model")
+
+    require(model.are_equal(r.+(i), id_morph(f.domain)), s"$r composed with $i is not identity")
+    require(model.are_equal(i.+(f), g.+(i)), s"$g composed with $i is not same as $i composed with $f")
+    require(model.are_equal(r.+(g), f.+(r)), s"$r composed with $g is not same as $f composed with $r")
+
+    require(model.fib.contains(g), s"$g is not a fibration in the category $model")
+
+    model.fib += f
+  }
+
+  def axiom_3_cofib(f : Morphism, g : Morphism, r : Morphism, i : Morphism) : Unit = {
+    require(model.morphisms.contains(f), s"$f is not a morphism in the category $model")
+    require(model.morphisms.contains(g), s"$g is not a morphism in the category $model")
+    require(model.morphisms.contains(i), s"$i is not a morphism in the category $model")
+    require(model.morphisms.contains(r), s"$r is not a morphism in the category $model")
+
+    require(model.are_equal(r.+(i), id_morph(f.domain)), s"$r composed with $i is not identity")
+    require(model.are_equal(i.+(f), g.+(i)), s"$g composed with $i is not same as $i composed with $f")
+    require(model.are_equal(r.+(g), f.+(r)), s"$r composed with $g is not same as $f composed with $r")
+
+    require(model.cofib.contains(g), s"$g is not a fibration in the category $model")
+
+    model.cofib += f
+  } 
+
+  /**        a
+   *     u ----> x
+   *   i |       | p
+   *     \/     \/
+   *     v ----> y
+   *         b
+   */
+  def axiom_4(a : Morphism, b : Morphism, i : Morphism, p : Morphism) : Unit = {
+
+    require(model.are_equal(p.+(a), b.+(i)), s"The diagram does not commute")
+    require(model.cofib.contains(i), s"$i is not a cofibration")
+    require(model.fib.contains(p),   s"$p is not a fibration")
+    require(model.weak_eqv.intersect(Set(i,p)).nonEmpty, s"Neither $i not $p is an weak equivalence")
+
+    val t : Morphism = new Atomic(b.domain, a.range)
+    model.morphisms += t
+    model.morphisms += p.+(t)
+    model.morphisms += t.+(i)
+    model.equality_of_morphisms.add(Set(p.+(t), b))
+    model.equality_of_morphisms.add(Set(t.+(i), a))
+  }
+
   def axiom_5_1(f : Morphism) : Unit = {
     require(model.morphisms.contains(f), s"$f is not a morphism in $model")
     val c : Object   = new Object() 
@@ -54,28 +145,6 @@ trait Model extends Category { model =>
     model.weak_eqv  += q
     model.equality_of_morphisms.add(Set(q.+(j), f))
   }
-
-  /**        a
-   *     u ----> x
-   *   i |       | p
-   *     \/     \/
-   *     v ----> y
-   *         b
-   */
-  def axiom_4(a : Morphism, b : Morphism, i : Morphism, p : Morphism) : Unit = {
-
-    require(model.are_equal(p.+(a), b.+(i)), s"The diagram does not commute")
-    require(model.cofib.contains(i), s"$i is not a cofibration")
-    require(model.fib.contains(p),   s"$p is not a fibration")
-    require(model.weak_eqv.intersect(Set(i,p)).nonEmpty, s"Neither $i not $p is an weak equivalence")
-
-    val t : Morphism = new Atomic(b.domain, a.range)
-    model.morphisms += t
-    model.morphisms += p.+(t)
-    model.morphisms += t.+(i)
-    model.equality_of_morphisms.add(Set(p.+(t), b))
-    model.equality_of_morphisms.add(Set(t.+(i), a))
-  }
 }
 
 object Model {
@@ -96,5 +165,16 @@ object Model {
       var equality_of_morphisms = new_equality
     }
   }
+
+  /**
+   *    f.domain
+   *       |
+   *     i |
+   *       |
+   *       \/
+   *       c --------> f.range
+   *             p
+   */
+  
 }
 
